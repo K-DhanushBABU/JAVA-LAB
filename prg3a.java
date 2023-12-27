a81738f50a04e024ab196e692ec203e78f8e0288
DHANUSH BABU <royalbrothers1618@gmail.com>
2023-12-27 19:41:17 +0530

    prg3.java

--- prg3a.java
+++ prg3a.java
@@ -1,5 +1,42 @@
 public class prg3a {
     public static void main(String[] args) {
+        int[] array = { 5, 3, 1, 4, 2 };
+        int n = array.length;
+        System.out.println("the array is ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
 
+        // ascending order
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                }
+            }
+
+        System.out.println("\nAscending order ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+        // descending order
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                }
+            }
+
+        System.out.println("\nDescending order");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
 }
